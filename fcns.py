3a8fb913af0aba694de6b2aab9f66444487d9f9d
danielherfurth <danielherfurth@gmail.com>
2021-10-24 03:14:23 -0400

    Adding all initial files.

---
+++ fcns.py
@@ -0,0 +1,66 @@
+# this file will be where i write all of my functions so that they dont clutter up the actual scripts
+
+
+def column_connector(col_name, df, return_df):
+    """
+    Args:
+        col_name (str): name of column in the raw data that you want to get the full name of.
+        df: the raw data dataframe
+        return_df: the dataframe containing the labels you want to return.
+
+    Returns: the column name from the labels dataframe.
+
+    Examples:
+        column_connector('age') returns 'Age bracket (in years)'
+
+        a = 'age'
+        column_connector('a') returns 'Age bracket (in years)'
+
+        column_connector(age) will raise an exception.
+    """
+    try:
+        col_idx = df.columns.get_loc(col_name)
+        returned_name = return_df.columns[col_idx]
+        return returned_name
+    except NameError:
+        print('the column name passed to the fcn must be a str or a variable holding a str.')
+
+
+def filter_data(df, filter_val, filter_column):
+    """
+
+    :param: df: the dataframe you want to search.
+    :param: filter_val: the value you want to find and keep visible after filtering.
+    :param: filter_column: the column you want to check for the value in
+    :return: the filtered dataframe only showing the values you want to see.
+
+    Examples:
+        filter_data(df_raw, 7, 'age') returns a dataframe only showing people >75 (because 7 is the int that
+        corresponds to >75)
+    """
+
+    filtered_df = df[df[filter_column] == filter_val]
+    return filtered_df
+
+
+def value_connector(lookup_val, lookup_col, return_col):
+    """
+
+    :param lookup_val: the value from the raw data you want to lookup
+    :param lookup_col: the name of the column holding your lookup value
+    :param return_col: the name of the column you want to pull the corresponding value from.
+    :return: the equivalent value from the labels column
+
+    Note: to avoid having to manually find the filter_column, use this fcn with column_connector.
+
+    Examples:
+        import fcns as f
+        f.value_connector(1, df_raw['age'], df_labels[(f.column_connector('age'))])
+
+        will return
+        The above uses the column_connector to retrieve the name of the df_labels column you're interested in.
+        This is much simpler than manually looking up and comparing column names.
+    """
+    output_dict = dict(zip(lookup_col.unique(), return_col.unique()))
+    output = output_dict[lookup_val]
+    return output
---
+++ prep_work.py
@@ -0,0 +1,52 @@
+# %%
+
+import pandas as pd
+import seaborn as sns
+import os
+import fcns as f
+
+fp = r'data/sources'
+
+df_raw = pd.read_excel(
+        os.path.join(fp, 'manuscript_raw_data.xlsx')
+)
+
+df_labels = pd.read_excel(
+        os.path.join(fp, 'labels_for_manuscript_raw_data.xlsx')
+)
+
+df_raw_us = f.filter_data(df_raw, 1, 'country')
+
+# %%
+
+# testing function
+# print(f.filter_data(7, 'age', df_raw))
+# f.value_connector(1, df_raw['age'], df_labels[(f.column_connector('age'))])
+#
+# connected = {}
+#
+# # make dict where k:v is raw_col:label_col
+# for col in df_raw.columns:
+#     connected[col] = f.column_connector(col)
+#
+# # make a df from the dict, then save to excel
+# k = list(connected.keys())
+# v = list(connected.values())
+#
+# d = pd.DataFrame.from_dict(connected.items())
+# d.to_excel('column_lookups.xlsx')
+# ! possible corr between obesity and fitness_health
+# %%
+
+# pickle files are significantly faster to read into dfs than csv files or excel files.
+# id def recommend you read these files into pandas in place of the xlsx or csv versions
+
+dfs = [df_raw, df_labels, df_raw_us]
+dfs_list = ['df_raw', 'df_labels', 'df_raw_us']
+zipped = dict(zip(dfs_list, dfs))
+
+for k,v in zipped.items():
+    filename = k
+    v.to_pickle(os.path.join('data', filename+ '.pickle'))
+
+#%%
